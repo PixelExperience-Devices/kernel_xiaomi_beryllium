2566ab6f7c96eb28b7fcb8181bf8d8f992b7cc73
Sajid Shahriar <63386478+sajidshahriar72543@users.noreply.github.com>
2023-01-17 20:10:51 +0600

    Import necessary scripts

---
+++ kernel_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 CloudedQuartz
+#
+
+# Script to set up environment to build an android kernel
+# Assumes required packages are already installed
+
+# Config
+CURRENT_DIR="$(pwd)"
+KERNELNAME="perf+"
+KERNEL_DIR="$CURRENT_DIR"
+AK_REPO="https://github.com/sajidshahriar72543/AnyKernel3"
+AK_DIR="$HOME/AnyKernel3"
+TC_DIR="$HOME/proton-proton"
+# End Config
+
+# clone_tc - clones proton clang to TC_DIR
+clone_tc() {
+	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
+}
+
+# Clones anykernel
+clone_ak() {
+	git clone $AK_REPO $AK_DIR
+}
+# Actually do stuff
+clone_tc
+clone_ak
+
+# Run build script
+. ${CURRENT_DIR}/kernel_build.sh
